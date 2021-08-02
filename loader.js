819de63d2bbb23f49d085f42e30e29a768d8bdfe
geetesh335 <73090898+geetesh335@users.noreply.github.com>
2021-08-02 19:38:48 +0530

    new file added
    vector icon file added

---
+++ loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const loader = () => {
+    return (
+        <View>
+            <Text>loder component</Text>
+        </View>
+    )
+}
+
+export default loader
